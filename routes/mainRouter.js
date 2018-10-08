717cb27a82426b432d2880312938a55dd90f17aa
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-08 06:08:57 +0530

    handled login request

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var uuid = require('uuid/v4');
+
+function generateCSRFToken() {
+    return uuid();
+}
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
-    if (req.session.username) {
-        res.render('../public/views/home');
+    if (req.cookies['username']) {
+        res.redirect('home')
     } else {
         res.redirect('login');
     }
@@ -17,10 +22,21 @@ router.get('/login', function (req, res, next) {
 
 /* GET home page. */
 router.get('/home', function (req, res, next) {
-    if (req.session.user) {
+    if (req.cookies['username']) {
         res.render('../public/views/home');
     } else {
         res.send(401);
+    }
+});
+
+/* POST login. */
+router.post('/login', function (req, res, next) {
+    if (req.body.username == "admin" && req.body.password == "admin") {
+        res.cookie('csrf_token', generateCSRFToken());
+        res.cookie('username', req.body.username);
+        res.redirect('home');
+    } else {
+        res.render('../public/views/login', {message: 'Invalid username or password!'});
     }
 });
 
